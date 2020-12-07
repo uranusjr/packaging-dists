d991fc3d39801e16254872571ffdd228c5197f5e
Philippe Ombredanne <pombredanne@nexb.com>
2020-12-07 16:51:58 +0100

    Add origin and license hints for pip code
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- src/packaging_dists/_utils.py
+++ src/packaging_dists/_utils.py
@@ -1,3 +1,24 @@
+# Copyright (c) 2008-2020 The pip developers (see AUTHORS.txt file)
+# 
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+# 
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 from packaging.utils import canonicalize_name
 
 from typing import Optional
@@ -7,8 +28,11 @@ def _find_name_version_sep(fragment, canonical_name):
     # type: (str, str) -> int
     """Find the separator's index based on the package's canonical name.
 
-    Copied verbatim from pip. This function is needed since the canonicalized
-    name does not necessarily have the same length as the egg info's name part.
+    Copied verbatim from:
+      pip._internal.index.package_finder._find_name_version_sep
+
+    This function is needed since the canonicalized name does not necessarily
+    have the same length as the egg info's name part.
     An example::
 
     >>> fragment = 'foo__bar-1.0'
@@ -31,7 +55,8 @@ def _extract_version_from_fragment(fragment, canonical_name):
     # type: (str, str) -> Optional[str]
     """Parse the version string from an sdist stem.
 
-    This is copied verbatim from pip.
+    This is copied verbatim from:
+       pip._internal.index.package_finder._extract_version_from_fragment
     """
     try:
         version_start = _find_name_version_sep(fragment, canonical_name) + 1
